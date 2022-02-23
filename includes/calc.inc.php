f566c7a5f4f9e02f6b3049dcfcbf2318e999e3a3
developer-j <87341943+jannatwdeveloper@users.noreply.github.com>
2022-02-23 21:39:48 +0600

    Add files via upload

---
+++ includes/calc.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+include 'class-autoload.inc.php';
+include 'db.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <div>
+        <h1>Results :<?php
+                        $oper = $_POST["oper"];
+                        $num1 = $_POST["num1"];
+                        $num2 = $_POST["num2"];
+
+                        $calc = new Calc($oper, (int)$num1, (int)$num2);
+
+                        try {
+                            echo $calc->calculator();
+                        } catch (TypeError $e) {
+                            echo "Error! :" . $e->getMessage();
+                        }
+                        ?>
+        </h1>
+        <a href="../index.php"><button>Home</button></a>
+    </div>
+
+</body>
+
+</html>
