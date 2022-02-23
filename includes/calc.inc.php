8d216e5c5a2590a8cc8b7b13ca9537949b135602
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2022-02-23 23:53:57 +0600

    CORS Policy Added by Erfan Ahmed SIam

--- includes/calc.inc.php
+++ includes/calc.inc.php
@@ -1,8 +1,12 @@
 <?php
-
 declare(strict_types=1);
 include 'class-autoload.inc.php';
 
+/*Code For CORS*/
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+/*Code For CORS*/
 
 ?>
 
